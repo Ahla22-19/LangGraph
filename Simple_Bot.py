07f07d93cafcec1a8412f5311c3fa4cd8ae67519
Ahla22-19 <zeynuahlam@gmail.com>
2026-01-21 16:03:29 +0300

    import all neccessary libraries

---
+++ Simple_Bot.py
@@ -0,0 +1,5 @@
+from typing import TypeDict, List
+from langchain_core.message import HumanMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
