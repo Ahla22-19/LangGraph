7f0fb8641ca45e86ac161979d7e488d9b4b37626
Ahla22-19 <zeynuahlam@gmail.com>
2026-01-21 16:12:39 +0300

    chore:.env file

--- Simple_Bot.py
+++ Simple_Bot.py
@@ -3,3 +3,7 @@ from langchain_core.message import HumanMessage
 from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph, START, END
 from dotenv import load_dotenv
+
+load.env()
+
+Class AgentState(TypeDict):
