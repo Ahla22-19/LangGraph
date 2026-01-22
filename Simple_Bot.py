81577458eb0dc0590bd5c622189a51521707d33b
Ahla22-19 <zeynuahlam@gmail.com>
2026-01-22 12:43:53 +0300

    Ignore venv and update bot code

--- Simple_Bot.py
+++ Simple_Bot.py
@@ -1,22 +1,31 @@
 from typing import TypedDict, List
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, AIMessage
 from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph, START, END
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
 class AgentState(TypedDict):
-    messages: List[HumanMessage]
+    messages: List[HumanMessage | AIMessage]
 
 llm = ChatOpenAI(model="gpt-4o")
 
 def process(state: AgentState) -> AgentState:
     response = llm.invoke(state["messages"])
-    return state
+    print(f"\nAI: {response.content}")
+    return {
+        "messages": state["messages"] + [response]
+    }
 
 graph = StateGraph(AgentState)
 graph.add_node("process", process)
 graph.add_edge(START, "process")
 graph.add_edge("process", END)
 agent = graph.compile()
+
+user_input = input("Enter: ")
+while user_input != "exit":
+    agent.invoke({"messages": [HumanMessage(content=user_input)]})
+    user_input = input("Enter: ")
 
